191762f8aad257185a902832091561056c39bca5
Andrey Tabakov <komdosh@yandex.ru>
2020-05-07 12:21:23 +0300

    resolve relative paths

--- src/app/pages/e-commerce/e-commerce.component.ts
+++ src/app/pages/e-commerce/e-commerce.component.ts
@@ -1,8 +1,11 @@
 import { Component } from '@angular/core';
+import {LocationStrategy} from '@angular/common';
 
 @Component({
   selector: 'ngx-ecommerce',
   templateUrl: './e-commerce.component.html',
 })
 export class ECommerceComponent {
+  constructor(public locationStrategy: LocationStrategy) {
+  }
 }
